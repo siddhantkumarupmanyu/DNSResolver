25a7bccbd29ec386d3ec790c37a3e92c6f10b04c
Siddhant Kumar <siddhantkumarupmanyu@gmail.com>
2021-04-23 23:09:38 +0530

    Add DNSQueryBuilder

---
+++ src/sku/dnsresolver/DNSQueryBuilder.java
@@ -0,0 +1,44 @@
+package sku.dnsresolver;
+
+public class DNSQueryBuilder {
+
+    private short id;
+    private boolean recursionDesired;
+    private DNSPacket.DNSQuery[] queries;
+
+    public DNSQueryBuilder setId(short id) {
+        this.id = id;
+        return this;
+    }
+
+    public DNSQueryBuilder setRecursionDesired(boolean recursionDesired) {
+        this.recursionDesired = recursionDesired;
+        return this;
+    }
+
+    public DNSQueryBuilder setQueries(DNSPacket.DNSQuery... queries) {
+        this.queries = queries;
+        return this;
+    }
+
+    public DNSPacket build() {
+        return new DNSPacketBuilder()
+                .setId(id)
+                .setResponse(false)
+                .setOpCode(0)
+                .setAuthoritative(false)
+                .setTruncated(false)
+                .setRecursionDesired(recursionDesired)
+                .setRecursionAvailable(false)
+                .setZ(false)
+                .setAnswerAuthenticated(false)
+                .setNonAuthenticatedData(false)
+                .setQuestionCount((short) 1)
+                .setAnswerRRCount((short) 0)
+                .setAuthorityRRCount((short) 0)
+                .setAdditionalRRCount((short) 0)
+                .setQueries(queries)
+                .setAnswers()
+                .build();
+    }
+}
---
+++ test/unit/sku/dnsresolver/DNSQueryBuilderTest.java
@@ -0,0 +1,47 @@
+package sku.dnsresolver;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+
+public class DNSQueryBuilderTest {
+
+
+    @Test
+    public void buildADNSQueryPacket() {
+        short id = 1;
+        DNSPacket.DNSQuery query = new DNSPacket.DNSQuery("www.example.com", (short) 1, (short) 1);
+        DNSQueryBuilder builder = new DNSQueryBuilder()
+                .setId(id)
+                .setRecursionDesired(true)
+                .setQueries(query);
+
+        assertThat(builder.build(), is(equalTo(dnsQueryPacketWith(id))));
+    }
+
+
+    private DNSPacket dnsQueryPacketWith(int id) {
+        DNSPacket.DNSQuery query = new DNSPacket.DNSQuery("www.example.com", (short) 1, (short) 1);
+        return new DNSPacket(
+                (short) id,
+                false,
+                0,
+                false,
+                false,
+                true,
+                false,
+                false,
+                false,
+                false,
+                (short) 1,
+                (short) 0,
+                (short) 0,
+                (short) 0,
+                new DNSPacket.DNSQuery[]{query},
+                new DNSPacket.DNSAnswer[0]
+        );
+    }
+}
